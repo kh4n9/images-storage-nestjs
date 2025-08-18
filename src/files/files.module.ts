f0e22f6da7ff6546abd556ad23a002c788afb8d7
Hoàng Minh Khang <30745739+kh4n9@users.noreply.github.com>
2025-08-18 16:56:25 +0700

    feat: refresh Discord file URLs hourly

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -4,13 +4,14 @@ import { FilesService } from './files.service';
 import { Files, FilesSchema } from './files.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DiscordStorageService } from '../common/services/discord-storage.service';
+import { FileUrlRefreshService } from './file-url-refresh.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Files.name, schema: FilesSchema }]),
   ],
   controllers: [FilesController],
-  providers: [FilesService, DiscordStorageService],
+  providers: [FilesService, DiscordStorageService, FileUrlRefreshService],
   exports: [FilesService, DiscordStorageService],
 })
 export class FilesModule {}
