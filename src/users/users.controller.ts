f65808fbe48338807351c455bf4378384e3b94f4
Hoàng Minh Khang <30745739+kh4n9@users.noreply.github.com>
2025-08-19 13:37:18 +0700

    feat(users): add role-protected routes

---
+++ src/users/users.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './users.schema';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Roles(Role.Admin)
+  create(@Body() user: User) {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  @Roles(Role.Admin)
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':email')
+  @Roles(Role.Admin)
+  findOne(@Param('email') email: string) {
+    return this.usersService.findOne(email);
+  }
+
+  @Patch(':id')
+  @Roles(Role.Admin)
+  update(@Param('id') id: string, @Body() user: Partial<User>) {
+    return this.usersService.update(id, user);
+  }
+
+  @Delete(':id')
+  @Roles(Role.Admin)
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
